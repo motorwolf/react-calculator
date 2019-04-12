af814e468bf9fe726e56dc2e99c4bbfad1c8b1ae
Kristen Payne <victorycabbage@gmail.com>
2019-04-12 10:21:18 -0700

    added some basic buttons to simple calculator

--- src/App.js
+++ src/App.js
@@ -2,25 +2,56 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Button = (props) => {
+  return (
+    <button onClick={() => {props.clickFunction(props.val)}}>{props.val}</button>
+  );
+}
+
 class App extends Component {
+  state = {
+    currentNumber: "0",
+    prevValue: "",
+  }
+  
+  addNumber = (n) => {
+    if(this.state.currentNumber === "0"){
+      this.setState({currentNumber: n});
+    }
+    else {
+      this.setState({currentNumber: this.state.currentNumber + n});
+    }
+  }
+
+  addDecimal = (n) => {
+    if(this.state.currentNumber.indexOf('.') === -1){
+        this.setState({currentNumber: this.state.currentNumber + n});
+    }
+  }
+
+  add = () => {
+    //this.setState({operator: })
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+      <h2>{this.state.prevValue}</h2>
+      <h2>{this.state.operator}</h2>
+      <h2>{this.state.currentNumber}</h2>
+      
+      <Button val="1" clickFunction={this.addNumber}/>
+      <Button val="2" clickFunction={this.addNumber}/>
+      <Button val="3" clickFunction={this.addNumber}/>
+      <Button val="4" clickFunction={this.addNumber}/>
+      <Button val="5" clickFunction={this.addNumber}/>
+      <Button val="6" clickFunction={this.addNumber}/>
+      <Button val="7" clickFunction={this.addNumber}/>
+      <Button val="8" clickFunction={this.addNumber}/>
+      <Button val="9" clickFunction={this.addNumber}/>
+      <Button val="0" clickFunction={this.addNumber}/>
+      <Button val="." clickFunction={this.addDecimal}/>
+      <Button val="+" clickFunction={this.add}/>
+      </>
     );
   }
 }
